a3418c3029828c32ea1d95d691b644812cf78092
jonirap <joni.rapoport@gmail.com>
2016-07-24 01:24:07 +0300

    Our very first simple web crawler
    this crawler starts at a certain webpage, and goes on crawling the web for a specified number of web pages. Each time a new address is reached, the crawler prints the 10 most common words in that page.

---
+++ Crawler/crawler/simple_crawler.py
@@ -0,0 +1,44 @@
+from urllib import parse
+from collections import Counter
+import re
+from html.parser import HTMLParser
+from urllib.request import urlopen
+
+
+class LinkParser(HTMLParser):
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            for (key, value) in attrs:
+                if key == 'href':
+                    new_url = parse.urljoin(self.base_url, value)
+                    self.links = self.links + [new_url]
+
+    def change_url(self, url):
+        self.base_url = url
+        response = urlopen(url)
+        self.data = response.read().decode("utf-8")
+        self.links = []
+
+    def parse_page(self):
+        self.feed(self.data)
+
+
+def spider(url, max_pages):
+    pages_to_visit = set(url)
+    number_visited = 0
+    parser = LinkParser()
+    while number_visited < max_pages and pages_to_visit != []:
+        url = pages_to_visit[number_visited]
+        number_visited += 1
+        print(number_visited, "Visiting:", url)
+        try:
+            parser.change_url(url)
+            parser.parse_page()
+            pages_to_visit |= set(parser.links)
+            words = re.findall(r'\w+', parser.data.lower())
+            print(Counter(words).most_common(10))
+        except:
+            print("!!fail!!")
+
+spider("https://www.facebook.com/", 20)
