5c92ca6ce1ff3fecd1e227107fcd41d16fcc812e
jonirap <joni.rapoport@gmail.com>
2016-07-24 14:18:12 +0300

    added tor

--- Crawler/crawler/simple_crawler.py
+++ Crawler/crawler/simple_crawler.py
@@ -1,8 +1,24 @@
-from urllib import parse
-from collections import Counter
 import re
+import socket
+import socks
+import stem.process
+import traceback
+from collections import Counter
 from html.parser import HTMLParser
+from stem.util import term
+from urllib import parse
 from urllib.request import urlopen
+
+SOCKS_PORT = 7000
+
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', SOCKS_PORT)
+socket.socket = socks.socksocket
+
+
+def getaddrinfo(*args):
+    return [(socket.AF_INET, socket.SOCK_STREAM, 6, '', (args[0], args[1]))]
+
+socket.getaddrinfo = getaddrinfo
 
 
 class LinkParser(HTMLParser):
@@ -16,19 +32,18 @@ class LinkParser(HTMLParser):
 
     def change_url(self, url):
         self.base_url = url
-        response = urlopen(url)
-        self.data = response.read().decode("utf-8")
+        self.data = urlopen(url).read().decode("utf-8")
         self.links = []
 
     def parse_page(self):
         self.feed(self.data)
 
 
-def spider(url, max_pages):
+def spider(url, max_pages, tor):
     pages_to_visit = set([url])
     number_visited = 0
     parser = LinkParser()
-    while number_visited < max_pages and pages_to_visit != []:
+    while number_visited < max_pages and number_visited < len(pages_to_visit):
         url = list(pages_to_visit)
         url = url[number_visited]
         number_visited += 1
@@ -40,6 +55,20 @@ def spider(url, max_pages):
             words = re.findall(r'\w+', parser.data.lower())
             print(Counter(words).most_common(10))
         except:
+            tor.kill()
             print("!!fail!!")
+            traceback.print_exc()
 
-spider("https://www.facebook.com/", 30)
+
+def print_bootstrap_lines(line):
+    # if "Bootstrapped " in line:
+        print(term.format(line, term.Color.BLUE))
+tor = stem.process.launch_tor_with_config(
+    tor_cmd='C:\\Users\\jonir_000\\Desktop\\Tor Browser\\Browser\\TorBrowser\\Tor\\tor.exe',
+    config={
+        'SocksPort': str(SOCKS_PORT)
+        },
+    init_msg_handler=print_bootstrap_lines
+)
+spider("https://www.facebook.com/", 2, tor)
+tor.kill()
